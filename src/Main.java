e435b546c3998d9b5afe30f260b9a667f4975b11
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-01-12 16:58:59 +0700

    Point 2D and point 3D

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D draw2D = new Point2D(2.5f, 4.4f);
+        System.out.println(draw2D);
+        Point3D draw3D = new Point3D(3.3f, 4.5f, 6.6f);
+        System.out.println(draw3D);
+    }
+}
